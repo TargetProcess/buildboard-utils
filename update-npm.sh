f1f7b003d08e7eaf4c8fe8155c2a3e5cc44111ce
Alex Fomin <ShurickFomin@gmail.com>
2016-01-18 14:56:59 +0300

    github mapping prototype

--- update-npm.sh
+++ update-npm.sh
@@ -3,10 +3,7 @@
 for path in "tool-tp" "tool-github" "tool-travis"
 do
     pushd ../$path
-    echo rm
-    rm -rf node_modules
-    echo install
-    npm install
+    npm update buildboard-tool-bootstrap
 
     popd
 done
